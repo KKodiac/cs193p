35a8e10e07cd7a11bbfd2ab0084b6a7eff150ec2
Sean Hong(홍성민) <seanhong2000@gmail.com>
2022-01-16 17:34:52 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sean Hong on 2022/01/16.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
